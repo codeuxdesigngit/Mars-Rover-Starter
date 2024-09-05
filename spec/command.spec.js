2d4b3a1f2d8b48eec2a8227f6b249f1ec5cc277d
Julia Kuprina <jkuprina@gmail.com>
2024-09-05 13:17:54 -0500

    Graded Assignment 3 Finished

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -1,11 +1,19 @@
 const Command = require("../command.js");
 
 describe("Command class", function () {
-  
   it("throws error if command type is NOT passed into constructor as the first parameter", function () {
-    // Checks for error when commandType is not provided
     expect(function () {
       new Command();
     }).toThrow(new Error("Command type required."));
+  });
+
+  it("constructor sets command type", function () {
+    const command = new Command("MOVE", 12000);
+    expect(command.commandType).toBe("MOVE");
+  });
+
+  it("constructor sets a value passed in as the 2nd argument", function () {
+    const command = new Command("MOVE", 12000);
+    expect(command.value).toBe(12000);
   });
 });
